fd3a224ab6e2e237d4be5b7fb52516d9cc4ac03b
henrichg <henrich.gron@gmail.com>
2022-11-21 19:41:22 +0100

    Change: Title for notification permission dialog.

--- pppPutSettings/src/main/java/sk/henrichg/pppputsettings/Permissions.java
+++ pppPutSettings/src/main/java/sk/henrichg/pppputsettings/Permissions.java
@@ -22,6 +22,7 @@ class Permissions {
                 if (!notificationManager.areNotificationsEnabled()) {
 
                     AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+                    dialogBuilder.setTitle(R.string.pppputsettings_app_name);
                     dialogBuilder.setMessage(R.string.pppputsettings_notifications_permission_text);
                     //dialogBuilder.setIcon(android.R.drawable.ic_dialog_alert);
                     dialogBuilder.setPositiveButton(R.string.pppputsettings_enable_notificaitons_button, (dialog, which) -> {
