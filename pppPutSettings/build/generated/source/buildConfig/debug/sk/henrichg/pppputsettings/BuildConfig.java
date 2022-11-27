572ec21d9a0213fb7463ef178f7a9be394a86094
henrichg <henrich.gron@gmail.com>
2022-11-27 22:15:11 +0100

    New version 1.0.1.1.

--- pppPutSettings/build/generated/source/buildConfig/debug/sk/henrichg/pppputsettings/BuildConfig.java
+++ pppPutSettings/build/generated/source/buildConfig/debug/sk/henrichg/pppputsettings/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "sk.henrichg.pppputsettings";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 26;
-  public static final String VERSION_NAME = "1.0.1.1b";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "1.0.1.1";
 }
