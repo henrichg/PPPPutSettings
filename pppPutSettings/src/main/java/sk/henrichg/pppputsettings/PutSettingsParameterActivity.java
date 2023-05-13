498c7fc2c7120e1f9537ad0c7ca10211ae73b03e
henrichg <henrich.gron@gmail.com>
2023-05-13 16:59:02 +0200

    Fixed warnings.

--- pppPutSettings/src/main/java/sk/henrichg/pppputsettings/PutSettingsParameterActivity.java
+++ pppPutSettings/src/main/java/sk/henrichg/pppputsettings/PutSettingsParameterActivity.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
