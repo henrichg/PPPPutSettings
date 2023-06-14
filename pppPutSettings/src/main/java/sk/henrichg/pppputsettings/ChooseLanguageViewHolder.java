ec3870520bdf1268d7b3517991d6cc7ac99bf20a
henrichg <henrich.gron@gmail.com>
2023-06-14 13:32:06 +0200

    Change: Fixed inner static classes.

---
+++ pppPutSettings/src/main/java/sk/henrichg/pppputsettings/ChooseLanguageViewHolder.java
@@ -0,0 +1,10 @@
+package sk.henrichg.pppputsettings;
+
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+class ChooseLanguageViewHolder {
+    RadioButton radioButton;
+    TextView languageLabel;
+    //int position;
+}
